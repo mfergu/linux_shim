c56065ec8d203ea1b4af15b5109667de8038d474
mfergu <mfergu3@g.clemson.edu>
2017-01-26 14:55:13 -0500

    added some wonky code

--- leakcount.c
+++ leakcount.c
@@ -2,12 +2,29 @@
 //http://bit.ly/2juhG81    
 #define _GNU_SOURCE    
 
+void __attribute__ ((constructor)) malloc_init(size_t);
+void __attribute__ ((destructor)) cleanup(void);
+
 #include <stdio.h>    
 #include <stdlib.h>    
 #include <dlfcn.h>    
     
-// intecepting malloc calls 
+void* (*original_malloc)(size_t) = NULL;
 
+//called when the library is unloaded
+void cleanup(void) {
+	//use this to function to close any files that were still open
+	//or do any other clean up.
+	printf("cleaning up.\n");
+}
+
+void malloc_init(void) {
+	if(original_malloc == NULL) {
+		original_malloc = dlsym(RTLD_NEXT, "malloc");
+	}
+	printf("initializing library.\n");
+}
+// intecepting malloc calls 
 void* malloc(size_t size) {    
     
     void* (*libc_malloc)(size_t) = dlsym(RTLD_NEXT, "malloc");    
