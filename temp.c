abafb632a6fea0f20d5d44c4e63f6b8de510f252
mfergu <mfergu3@g.clemson.edu>
2017-02-04 15:05:09 -0500

    shim working stderr not finished

--- temp.c
+++ temp.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
 int main(void) {
 
-	char* buf1 = malloc(2*sizeof(char));
-	char* buf2 = malloc(2*sizeof(char));
+	char* buf1 = malloc(10*sizeof(char));
+	char* buf2 = malloc(20*sizeof(char));
+	char* buf3 = malloc(30*sizeof(char));
+	char* buf4 = malloc(40*sizeof(char));
+	char* buf5 = malloc(50*sizeof(char));
+	char* buf6 = malloc(60*sizeof(char));
+/*
+	printf("\tbuf1 location is %p\n\n", buf1);
+	printf("\tbuf2 location is %p\n\n", buf2);
+	printf("\tbuf3 location is %p\n\n", buf3);
+*/
 	free(buf2);
-	free(buf1);
 	return 0;
 }
